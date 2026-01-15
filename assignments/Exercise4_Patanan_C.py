5c044f6f4803e000a50140fa18f0185524af83aa
PatananChai <patanan1997@gmail.com>
2026-01-15 13:28:44 +0800

    Create Exercise 4
    Exercise 4: ปฏิบัติการทดสอบการใช้งานตัวแปร

---
+++ assignments/Exercise4_Patanan_C.py
@@ -0,0 +1,11 @@
+FoundationEnglishScore = 90
+GeneralBusinessScore = 95
+IntroductionComputerScore = 95.5
+CompyerProgramingScore = 90
+
+print("--- Your Scores ---")
+print("Foundation English:", FoundationEnglishScore)
+print("General Business:", GeneralBusinessScore)
+print("Introduction to Computer Systems:", IntroductionComputerScore)
+print("Computer Programming:", CompyerProgramingScore)
+
