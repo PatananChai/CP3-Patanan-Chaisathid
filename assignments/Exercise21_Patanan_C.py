5657d172a9c1fbe97b362ca43ed09bf2fe142981
PatananChai <patanan1997@gmail.com>
2026-01-29 23:22:25 +0800

    Create Exercise21
    Exercise21 เรียนรู้การสร้างโปรแกรม GUI

---
+++ assignments/Exercise21_Patanan_C.py
@@ -0,0 +1,41 @@
+from tkinter import *
+import math
+
+def leftClickButton(event):
+    bmi = float(textBoxWeight.get())/math.pow((float(textBoxHeight.get())/100),2)
+    print(bmi)
+    if bmi >= 30:
+        labelResult.configure(text="อ้วนมาก")
+    elif bmi >= 25:
+        labelResult.configure(text="อ้วน")
+    elif bmi >= 23:
+        labelResult.configure(text="น้ำหนักเกิน")
+    elif bmi >= 18.6:
+        labelResult.configure(text="น้ำหนักปกติ")
+    else:
+        labelResult.configure(text="ผอมเกินไป")
+
+
+
+
+
+MainWindow = Tk()
+labelHeight = Label(MainWindow,text = "ส่วนสูง cm")
+labelHeight.grid(row = 0, column = 0)
+textBoxHeight = Entry(MainWindow)
+textBoxHeight.grid(row = 0, column = 1)
+
+
+labelWeight = Label(MainWindow,text = "น้ำหนัก kg")
+labelWeight.grid(row = 1, column = 0)
+textBoxWeight = Entry(MainWindow)
+textBoxWeight.grid(row = 1, column = 1)
+
+calculateButton = Button(MainWindow,text = "คำนวณ")
+calculateButton.bind("<Button-1>", leftClickButton)
+calculateButton.grid(row=2,column = 0)
+
+labelResult = Label(MainWindow,text="ผลลัพธ์")
+labelResult.grid(row = 2, column = 1)
+
+MainWindow.mainloop()
